38e892289c4967d6e17762ac373cfde480417617
SauHanChan <schan253@my.centennialcollege.ca>
2023-06-02 20:50:54 -0400

    seventh commit-Final edits before 1st cloud hosting and comments added

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
+/*
+  Filename: app.js;
+  Author: Sau Han, Chan, & default;
+  StudentID: 301276498;
+  Date: 2023-06-02;
+  Description: This is for configuring the whole express application (inc loading all the modules).
+*/
+
+//#To load all required modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//#To import the index.js & user.js file from the routes directory
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
+//#To create an instance of express application (for configuring the application and setting up routes)
 var app = express();
 
-// view engine setup
+//##view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//#To mount middleware functions to a non-specific path
+app.use(logger('dev'));   
+app.use(express.json());   
+app.use(express.urlencoded({ extended: false }));   
+app.use(cookieParser());   
+app.use(express.static(path.join(__dirname, 'public')));   
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter);     //#req to the root directory will be handled by indexRouter
 
-// #Form submission
+//#For form submission
 app.get('/',function(req,res,next){
   console.log(req.body);
 });
 
-// catch 404 and forward to error handler
+//##catch 404 and forward to error handler (below)
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// ##error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // ##set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // ##render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+//#To export the Express application instance for other modules' use
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* #GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
-/* GET about page. */
+/* #GET about page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
-/* GET projects page. */
+/* #GET projects page. */
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects' });
 });
-/* GET services page. */
+/* #GET services page. */
 router.get('/services', function(req, res, next) {
   res.render('services', { title: 'Services' });
 });
-/* GET contact page. */
+/* #GET contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
